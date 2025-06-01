4c4c527d07601d68731e025bae9b121bf94f33ae
paakofiaidoo <paakofiaidoo17@gmail.com>
2025-06-01 19:43:00 +0000

    fix: fixed ui
    Signed-off-by: paa-kofi aidoo

--- android/app/src/main/java/com/example/probably_a_scam/MainActivity.java
+++ android/app/src/main/java/com/example/probably_a_scam/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.probably_a_scam;
+package tech.paakofi.probably_a_scam;
 
 import io.flutter.embedding.android.FlutterActivity;
 
--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,11 @@ import 'package:probably_a_scam/scam_analyzer_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Probably A Scam',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
-        useMaterial3: true,
-      ),
-      home: const ScamAnalyzerScreen(),
-    );
-  }
-}
-runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp(Set<dynamic> set, {super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
